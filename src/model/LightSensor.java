7d0ad0d356d572682e8993040dea88dacfa839a7
fabian_mengerink@hotmail.com <fabian_mengerink@hotmail.com@29f838ed-e48b-e832-b481-986bef2153ea>
2010-12-08 09:45:44 +0000

    light rename

---
+++ src/model/LightSensor.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.Date;
+
+public class LightSensor {
+	private Date date;
+	private String time;
+	private String temp;
+
+	public LightSensor(Date date, String time, String temp) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
