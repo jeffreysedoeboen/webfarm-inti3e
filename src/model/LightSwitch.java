a9f0a988152caec2d5e4f6a01c334e3c4769e4b0
fabian_mengerink@hotmail.com <fabian_mengerink@hotmail.com@29f838ed-e48b-e832-b481-986bef2153ea>
2010-12-08 10:03:16 +0000

    LightSwitch

---
+++ src/model/LightSwitch.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Date;
+
+public class LightSwitch {
+
+	public LightSwitch(Date date, String time, String light) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
