f1a68542bb0537323167d8087c7656c1a1eafa75
bappie6@msn.com <bappie6@msn.com@29f838ed-e48b-e832-b481-986bef2153ea>
2011-01-06 13:30:16 +0000

    oops vergeten statistics servlet tecomitten

---
+++ src/com/analytics/web/Statistics.java
@@ -0,0 +1,51 @@
+package com.analytics.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.analytics.data.HitsDAO;
+
+/**
+ * Servlet implementation class Statistics
+ */
+public class Statistics extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Statistics() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String selectedIp = request.getParameter("selectedIp");
+		HitsDAO hits = new HitsDAO();
+		JSONObject json = new JSONObject();
+		
+		try {
+			json.put("pages", hits.getPagesByIp(selectedIp));
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(json.toString());
+		response.getWriter().print(json.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
