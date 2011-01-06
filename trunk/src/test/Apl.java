71d2771f31a7041571a45be782e4949d8a8537f5
bappie6@msn.com <bappie6@msn.com@29f838ed-e48b-e832-b481-986bef2153ea>
2011-01-06 08:41:57 +0000

    DBManager uit analytics verwijderd en apl verplaatst naar test

--- trunk/src/test/Apl.java
+++ trunk/src/test/Apl.java
@@ -1,11 +1,20 @@
 package test;
 
+import com.inti3e.database.dao.TempDao;
 import com.inti3e.model.DataManager;
+import com.inti3e.model.Temperature;
 
 
 public class Apl {
 
 	public static void main( String[] args ) {
-		DataManager dm = DataManager.getInstance();
+//		DataManager dm = DataManager.getInstance();
+		TempDao d = new TempDao();
+		for(Temperature t: d.getAllTemps()){
+			System.out.println(t.getDate());
+			System.out.println(t.getTemp());
+			System.out.println(t.getTime());
+			System.out.println();
+		}
 	}
 }
